d79471fe6d3ef53dfd24764d450b1d4e6ff9639f
PhilipYip1988 <56493395+PhilipYip1988@users.noreply.github.com>
2022-11-20 11:46:32 +0000

    Add files via upload

---
+++ 012_math/errorfunctions.py
@@ -0,0 +1,23 @@
+import math
+from math import pi, tau
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style("whitegrid")
+
+def error_function(z):
+    value = 0
+    for n in range(100):
+        value += (2 / math.sqrt(pi)) * (-1) ** n * z ** (2 * n + 1)/ ((math.factorial(n)*(2 * n + 1)))
+    return value
+
+
+def cerror_function(z):
+    return 1 - error_function(z)
+
+
+a = error_function(2)
+a2 = math.erf(2)
+b = cerror_function(2)
+b2 = math.erfc(2)
