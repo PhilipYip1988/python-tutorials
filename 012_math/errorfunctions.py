1827a3257ea7b9c30d641054437e325fd75596d3
PhilipYip1988 <56493395+PhilipYip1988@users.noreply.github.com>
2022-11-20 12:21:22 +0000

    Add files via upload

--- 012_math/errorfunctions.py
+++ 012_math/errorfunctions.py
@@ -9,7 +9,7 @@ sns.set_style("whitegrid")
 def error_function(z):
     value = 0
     for n in range(100):
-        value += (2 / math.sqrt(pi)) * (-1) ** n * z ** (2 * n + 1)/ ((math.factorial(n)*(2 * n + 1)))
+        value += (2 / math.sqrt(pi)) * (-1) ** n * z ** (2 * n + 1)/((math.factorial(n)*(2 * n + 1)))
     return value
 
 
@@ -24,4 +24,8 @@ b2 = math.erfc(2)
 
 """
 the error function erf(x) is the probability that a number drawn at random from the standard normal distribution (mean =0, standard deviation = 1) will be no greater than x.
+"""
+
+"""
+https://mathworld.wolfram.com/Erf.html
 """
